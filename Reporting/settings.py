704dbf50ab8efa567033fdbb18c0e55a742215b8
Nyahende <michaelnyahende8@gmail.com>
2024-08-31 17:02:24 +0300

    templates directory

--- Reporting/settings.py
+++ Reporting/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['reporting-un32.onrender.com']
 
+# '
 
 # Application definition
 
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'Reporting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
